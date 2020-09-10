81d06f54c92f420a72eb46683876712494a8d9be
José Ulombe <jose.ulombe@hotmail.com>
2020-09-10 22:36:56 +0100

    Add file

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ server.get("/", function(req, res){
     return res.render('index')
 })
 
-server.use(function(req,res) {
-    res.status(404).render("not-found")
-})
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+});
 
 
 //starting server
