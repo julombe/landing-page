b20e419a6240efcfd4e60575403617e75b931c4f
José Ulombe <jose.ulombe@hotmail.com>
2020-09-10 22:14:42 +0100

    Add file

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
 
 //starting server
 server.listen(3000, function(){
     console.log("server is running")
+})
+
+
+//configuring template engine nunjucks
+server.set("view engine", "html")
+nunjucks.configure("views", {
+    express: server
+})
+
+//adding routes
+server.get("/", function(req, res){
+    return res.render('index')
 })
