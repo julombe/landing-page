287405e5303cf8d0c295c79f3cdc601f1e0006a3
José Ulombe <jose.ulombe@hotmail.com>
2020-11-17 00:50:53 +0100

    add routes

--- routes.js
+++ routes.js
@@ -2,5 +2,13 @@ const express = require('express')
 const routes = require('routes')
 
 
+//creating routes
+routes.get("/", function(req, res){
+    return res.render('index')
+})
+
+routes.use(function(req, res) {
+    res.status(404).render("not-found");
+});
 
 module.exports = routes
